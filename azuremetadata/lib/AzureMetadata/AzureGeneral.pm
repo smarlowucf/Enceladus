3e115eb88a9c10b2436332e5c1b68b628991a9d3
Marcus Schäfer <ms@suse.com>
2015-06-18 09:42:46 +0200

    Fixed behaviour and added new instance information
    The --instance-name option should return the name and not the
    incarnation tag. Along with the change a new option to obtain
    the incarnation tag with --instance-tag and an option to obtain
    the instance id as representation of the smbios UUID
    with --instance-id has been added

--- azuremetadata/lib/AzureMetadata/AzureGeneral.pm
+++ azuremetadata/lib/AzureMetadata/AzureGeneral.pm
@@ -20,13 +20,78 @@ use warnings;
 use XML::LibXML;
 
 our @ISA    = qw (Exporter);
-our @EXPORT_OK = qw (get_instance_name);
+our @EXPORT_OK = qw (
+    get_instance_name
+    get_instance_incarnation_tag
+    get_instance_id
+);
 
 sub get_instance_name {
     my $xml = shift;
-    my @instance = $xml->getElementsByTagName('Incarnation');
-    my $name = $instance[0]->getAttribute('instance');
+    my @instances = $xml->getElementsByTagName('Instance');
+    my $name = $instances[0]->getAttribute('id');
     return $name;
+}
+
+sub get_instance_incarnation_tag {
+    my $xml = shift;
+    my @instance = $xml->getElementsByTagName('Incarnation');
+    my $tag = $instance[0]->getAttribute('instance');
+    return $tag;
+}
+
+sub get_instance_id {
+    my $smbios = qx(dmidecode | grep UUID | cut -c8-);
+    my $id = _fixup_guid_endianness($smbios);
+    return $id;
+}
+
+sub _fixup_guid_endianness {
+    # /.../
+    # Convert a string in the expected format, into 16 bytes,
+    # emulating .Net's Guid constructor
+    # ----
+    my $id   = shift;
+    chomp($id);
+    my $hx   = '[0-9a-f]';
+    if ($id !~ /^($hx{8})-($hx{4})-($hx{4})-($hx{4})-($hx{12})$/i) {
+        return;
+    }
+    my @parts = split (/-/,$id);
+    #==========================================
+    # pack into signed long 4 byte
+    #------------------------------------------
+    my $p1 = $parts[0];
+    $p1 = pack   'H*', $p1;
+    $p1 = unpack 'l>', $p1;
+    $p1 = pack   'l' , $p1;
+    $p1 = unpack 'H*', $p1;
+    #==========================================
+    # pack into unsigned short 2 byte
+    #------------------------------------------
+    my $p2 = $parts[1];
+    $p2 = pack   'H*', $p2;
+    $p2 = unpack 'S>', $p2;
+    $p2 = pack   'S' , $p2;
+    $p2 = unpack 'H*', $p2;
+    #==========================================
+    # pack into unsigned short 2 byte
+    #------------------------------------------
+    my $p3 = $parts[2];
+    $p3 = pack   'H*', $p3;
+    $p3 = unpack 'S>', $p3;
+    $p3 = pack   'S' , $p3;
+    $p3 = unpack 'H*', $p3;
+    #==========================================
+    # pack into hex string (high nybble first)
+    #------------------------------------------
+    my $p4 = $parts[3];
+    my $p5 = $parts[4];
+    #==========================================
+    # concat result and return
+    #------------------------------------------
+    my $guid = "$p1-$p2-$p3-$p4-$p5";
+    return $guid;
 }
 
 1;
